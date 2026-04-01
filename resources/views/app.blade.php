d8314b5daa9ecc610a507f83ccff32a2127c98fa
Antonio Fernandez <tfernandez.sc2@gmail.com>
2026-04-01 13:42:03 -0400

    added SVG favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,6 +6,11 @@
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <title>Email Exposure Assessment | MeteorTel Security</title>
     <meta name="description" content="Find out if your email has been exposed in a data breach and assess your domain's email security configuration." />
+    @if(env('FAVICON_PATH'))
+        <link rel="icon" href="{{ env('FAVICON_PATH') }}" type="{{ env('FAVICON_TYPE', 'image/x-icon') }}" />
+    @else
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+    @endif
     <script>
         window.appConfig = {
             discoveryCallUrl: @json(env('DISCOVERY_CALL_URL', 'https://meteortel.com/discovery-call/')),
